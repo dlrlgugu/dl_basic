6794276413e1d48db08fee9fa78c4a6529c398c1
dlrlgugu <hckguja@gmail.com>
2018-05-29 12:47:26 -0700

    added sigmoid_derivative
    Signed-off-by: dlrlgugu <hckguja@gmail.com>

--- nn_basics.py
+++ nn_basics.py
@@ -5,7 +5,12 @@ def sigmoid(x):
     s = 1/(1+np.exp(-x))
     return s
 
-print (sigmoid(3))
-
 x = np.array([1,2,3])
 print (sigmoid(x))
+
+def sigmoid_derivative(x):
+    s = sigmoid(x)
+    ds = s*(1-s)
+    return ds
+
+print (sigmoid_derivative(x))
